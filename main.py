0ac5d1b12a03a4f957886f5bf84c2e06433da744
Daemon-Open <149646642+Daemon-Open@users.noreply.github.com>
2023-11-07 16:20:39 +0000

    main.py
    v1.1.0

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import sys
 from PyQt5.QtCore import QUrl
-from PyQt5.QtWidgets import QApplication, QMainWindow, QToolBar, QAction, QSizePolicy, QLineEdit, QPushButton, QVBoxLayout, QWidget
-from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage, QWebEngineProfile, QWebEngineSettings
+from PyQt5.QtWidgets import QApplication, QMainWindow, QToolBar, QAction, QSizePolicy, QLineEdit, QPushButton, QVBoxLayout, QWidget, QTabWidget
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage, QWebEngineSettings
 from PyQt5 import QtGui
 import qdarkstyle
-import os
 
 class AdBlockingPage(QWebEnginePage):
     def __init__(self, ad_domains):
@@ -26,80 +25,14 @@ class AdBlockingPage(QWebEnginePage):
         return True  # Allow the request
 
 class WebBrowser(QMainWindow):
+
     def __init__(self):
         super().__init__()
 
-        self.ad_blocker_profile = None
-        self.browser = QWebEngineView()
-        self.browser.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-
-        settings = self.browser.settings()
-        settings.setAttribute(QWebEngineSettings.JavascriptEnabled, True)  # Enable JavaScript
-        settings.setAttribute(QWebEngineSettings.LocalStorageEnabled, False)  # Disable local storage
-        settings.setAttribute(QWebEngineSettings.LocalContentCanAccessRemoteUrls,
-                              False)  # Limit local content access to remote URLs
-        settings.setAttribute(QWebEngineSettings.PluginsEnabled, False)  # Disable plugins
-        settings.setAttribute(QWebEngineSettings.JavascriptCanOpenWindows, False)  # Disable pop-up windows
-        settings.setAttribute(QWebEngineSettings.XSSAuditingEnabled, True)  # Enable XSS auditing
-
+        self.tab_widget = QTabWidget()
+        self.tab_widget.setTabsClosable(True)
+        self.tab_widget.tabCloseRequested.connect(self.close_tab)
         self.init_ui()
-
-    def init_ui(self):
-        self.browser.setUrl(QUrl("https://duckduckgo.com/"))
-        print("Init UI - Default homepage URL:", self.browser.url().toString())
-        self.setGeometry(100, 100, 1024, 768)
-        self.setWindowTitle("Daemon Search")
-        self.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
-        self.setup_toolbar()
-        self.setup_main_widget()
-        self.centralWidget().setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
-        self.browser.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
-        self.browser.page().setBackgroundColor(QtGui.QColor(30, 30, 30))  # Set the background color
-        
-        # Read ad domains from the text file and store them in a list
-        self.ad_domains = self.read_ad_domains_from_file("adservers.txt")
-
-        self.page = AdBlockingPage(self.ad_domains)
-        self.browser.setPage(self.page)
-
-    def setup_toolbar(self):
-        nav_toolbar = QToolBar("Navigation")
-        self.addToolBar(nav_toolbar)
-
-        back_btn = QAction("Back", self)
-        back_btn.setStatusTip("Back to the previous page")
-        back_btn.triggered.connect(self.browser.back)
-        nav_toolbar.addAction(back_btn)
-
-        forward_btn = QAction("Forward", self)
-        forward_btn.setStatusTip("Forward to the next page")
-        forward_btn.triggered.connect(self.browser.forward)
-        nav_toolbar.addAction(forward_btn)
-
-        reload_btn = QAction("Reload", self)
-        reload_btn.setStatusTip("Reload the page")
-        reload_btn.triggered.connect(self.browser.reload)
-        nav_toolbar.addAction(reload_btn)
-
-        home_btn = QAction("Home", self)
-        home_btn.setStatusTip("Go to the homepage")
-        home_btn.triggered.connect(self.navigate_home)
-        nav_toolbar.addAction(home_btn)
-
-        self.url_input = QLineEdit(self)
-        self.url_input.returnPressed.connect(self.navigate_to_url)
-        nav_toolbar.addWidget(self.url_input)
-
-        go_btn = QPushButton("Go", self)
-        go_btn.clicked.connect(self.navigate_to_url)
-        nav_toolbar.addWidget(go_btn)
-
-    def setup_main_widget(self):
-        main_widget = QWidget(self)
-        main_layout = QVBoxLayout()
-        main_layout.addWidget(self.browser)
-        main_widget.setLayout(main_layout)
-        self.setCentralWidget(main_widget)
 
     def read_ad_domains_from_file(self, filename):
         ad_domains = []
@@ -111,22 +44,122 @@ class WebBrowser(QMainWindow):
             print(f"File {filename} not found. No ad domains will be blocked.")
         return ad_domains
 
+    def init_ui(self):
+        self.setGeometry(100, 100, 1024, 768)
+        self.setWindowTitle("Daemon Search")
+        self.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+        self.setCentralWidget(self.tab_widget)
+        self.setWindowIcon(QtGui.QIcon("mask.png"))
+
+        self.setup_toolbar()
+
+    def create_new_browser_instance(self, url=None):
+        new_browser = QWebEngineView()
+        new_browser.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        new_browser.settings().setAttribute(QWebEngineSettings.JavascriptEnabled, True)
+        new_browser.settings().setAttribute(QWebEngineSettings.LocalStorageEnabled, False)
+        new_browser.settings().setAttribute(QWebEngineSettings.LocalContentCanAccessRemoteUrls, False)
+        new_browser.settings().setAttribute(QWebEngineSettings.PluginsEnabled, False)
+        new_browser.settings().setAttribute(QWebEngineSettings.JavascriptCanOpenWindows, False)
+        new_browser.settings().setAttribute(QWebEngineSettings.XSSAuditingEnabled, True)
+        new_browser.setPage(AdBlockingPage(self.read_ad_domains_from_file("adservers.txt")))
+        new_browser.titleChanged.connect(self.update_tab_name)
+
+        if url:
+            new_browser.setUrl(QUrl(url))
+        else:
+            new_browser.setUrl(QUrl("https://duckduckgo.com"))
+
+        return new_browser
+
+    def add_new_tab(self, url=None):
+        new_browser = self.create_new_browser_instance(url)
+        new_browser.titleChanged.connect(self.update_tab_name)  # Connect to the update_tab_name method of WebBrowser
+        self.tab_widget.addTab(new_browser, "Loading...")
+
+    # Add this method inside the WebBrowser class
+    def update_tab_name(self, title):
+        index = self.tab_widget.indexOf(self.sender())  # Use self.sender() to get the current browser
+        if index != -1:
+            self.tab_widget.setTabText(index, title)
+
+    def setup_toolbar(self):
+        nav_toolbar = QToolBar("Navigation")
+        self.addToolBar(nav_toolbar)
+
+        new_tab_btn = QAction("New Tab", self)
+        new_tab_btn.setStatusTip("Open a new tab")
+        new_tab_btn.triggered.connect(self.add_new_tab)
+        nav_toolbar.addAction(new_tab_btn)
+
+        back_btn = QAction("Back", self)
+        back_btn.setStatusTip("Back to the previous page")
+        back_btn.triggered.connect(self.navigate_back)
+        nav_toolbar.addAction(back_btn)
+
+        forward_btn = QAction("Forward", self)
+        forward_btn.setStatusTip("Forward to the next page")
+        forward_btn.triggered.connect(self.navigate_forward)
+        nav_toolbar.addAction(forward_btn)
+
+        reload_btn = QAction("Reload", self)
+        reload_btn.setStatusTip("Reload the page")
+        reload_btn.triggered.connect(self.reload_page)
+        nav_toolbar.addAction(reload_btn)
+
+        home_btn = QAction("Home", self)
+        home_btn.setStatusTip("Go to the homepage")
+        home_btn.triggered.connect(self.navigate_home)
+        nav_toolbar.addAction(home_btn)
+
+        self.url_input = QLineEdit(self)
+        self.url_input.returnPressed.connect(self.navigate_to_url)
+        nav_toolbar.addWidget(self.url_input)
+
+        go_btn = QPushButton("Search", self)
+        go_btn.clicked.connect(self.navigate_to_url)
+        nav_toolbar.addWidget(go_btn)
+
+    def update_tab_name(self, title):
+        index = self.tab_widget.currentIndex()
+        if index != -1:
+            self.tab_widget.setTabText(index, title)
+
+    def close_tab(self, index):
+        if 0 <= index < self.tab_widget.count():
+            self.tab_widget.removeTab(index)
+
+    def navigate_back(self):
+        current_browser = self.tab_widget.currentWidget()
+        if current_browser:
+            current_browser.back()
+
+    def navigate_forward(self):
+        current_browser = self.tab_widget.currentWidget()
+        if current_browser:
+            current_browser.forward()
+
+    def reload_page(self):
+        current_browser = self.tab_widget.currentWidget()
+        if current_browser:
+            current_browser.reload()
+
     def navigate_home(self):
-        self.browser.setUrl(QUrl("https://duckduckgo.com/"))
+        current_browser = self.tab_widget.currentWidget()
+        if current_browser:
+            current_browser.setUrl(QUrl("https://duckduckgo.com"))
 
     def navigate_to_url(self):
         url = self.url_input.text()
         if not url.startswith("http") and not url.startswith("https"):
             url = "https://" + url
-        self.browser.setUrl(QUrl(url))
+        current_browser = self.tab_widget.currentWidget()
+        if current_browser:
+            current_browser.setUrl(QUrl(url))
 
-def main():
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    app.setWindowIcon(QtGui.QIcon("mask.png"))
     browser = WebBrowser()
     browser.show()
     app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
     sys.exit(app.exec_())
-
-if __name__ == "__main__":
-    main()
